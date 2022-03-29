74b1c75c1bdbcd41b6767113e15ae18fa910bd8f
uohzey <420709076@qq.com>
2022-03-29 22:28:18 +0800

    登陆界面设计

---
+++ dbms/src/components/LoginForm.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="login-form">
+    <!-- 登录表单头部 -->
+    <header class="login-title">
+      <h2>登录</h2>
+      <h4>
+        你尚未拥有账户?点击
+        <span class="login-regist">注册</span>
+        进行登录
+      </h4>
+    </header>
+    <!-- 登录表单内容 -->
+    <article>
+      <el-form
+        :model="LoginFormData"
+        :rules="rules"
+        class="login-form-body"
+        ref="LoginFormData"
+        label-width="100px"
+      >
+        <el-form-item label="手机号" prop="phone">
+          <el-input
+            v-model="LoginFormData.phone"
+            class="login-form-input"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="用户密码" prop="password">
+          <el-input
+            v-model="LoginFormData.password"
+            class="login-form-input"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <el-checkbox v-model="ready" class="login-ready"
+        >我已阅读并同意
+        <span>相关协议</span>
+      </el-checkbox>
+      <div class="login-button">
+        <el-button round type="primary">登录</el-button>
+      </div>
+      <!-- <el-form-item type = "text" aria-autocomplete="false" v-model=""></el-form-item> -->
+    </article>
+    <!-- 登录表单底部 -->
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      LoginFormData: {
+        phone: "",
+        password: "",
+      },
+      rules: {
+        phone: [
+          { required: true, message: "请输入手机号码", trigger: "blur" },
+          { min: 11, max: 11, message: "请输入手机号码 ", trigger: "blur" },
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
+      ready: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-form {
+  .login-title {
+    h2 {
+      font-size: 25px;
+    }
+    h4 {
+      font-size: 14px;
+      color: rgba($color: gray, $alpha: 0.7);
+      letter-spacing: 1px;
+      padding-top: 5px;
+      .login-regist {
+        color: #426ab3;
+      }
+      .login-regist:hover {
+        color: #6a6da9;
+      }
+    }
+  }
+  article {
+    .login-form-body {
+      width: 80%;
+      padding: 10px;
+      .login-form-input {
+        height: 20px;
+      }
+    }
+    .login-ready {
+      span {
+        color: #426ab3;
+      }
+      span:hover {
+        color: #6a6da9;
+      }
+    }
+    .login-button {
+      text-align: right;
+      padding-top: 15px;
+      padding-right: 15%;
+      button {
+        width: 40%;
+      }
+    }
+  }
+}
+</style>
---
+++ dbms/src/views/LoginInter.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <!-- background -->
+    <img :src="login_background" class="login-img" />
+    <div class="login-card">
+      <div class="login-bg"></div>
+      <login-form class="login-form"></login-form>
+    </div>
+  </div>
+  <!-- <img :src="login_background" /> -->
+</template>
+ 
+<script>
+import LoginForm from "../components/LoginForm.vue";
+import login_background from "../assets/images/login/login_background.jpg";
+export default {
+  name: "LoginInter",
+  components: { LoginForm },
+  data() {
+    return {
+      login_background,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  .login-img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+  .login-card {
+    width: 40%;
+    height: 50%;
+    position: absolute;
+    right: 8%;
+    top: 30%;
+    display: flex;
+    padding: 20px;
+    justify-content: center;
+    .login-bg {
+      width: 50%;
+      background-color: rgba($color: #fff, $alpha: 0.4);
+      border-radius: 10px 0px 0px 10px;
+    }
+    .login-form {
+      width: 50%;
+      min-width: 250px;
+      min-height: 360px;
+      background-color: #fff;
+      padding: 10px;
+      border-radius: 0 10px 10px 0;
+    }
+  }
+}
+</style>
