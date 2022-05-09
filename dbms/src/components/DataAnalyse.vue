1c6c533574c68625371bd7c6eac07b14a42f1c04
uohzey <420709076@qq.com>
2022-05-09 20:15:35 +0800

    data-analyse-component

---
+++ dbms/src/components/DataAnalyse.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-descriptions
+    class="margin-top"
+    title="数值分析"
+    :column="3"
+    :size="size"
+    border
+  >
+    <el-descriptions-item>
+      <template slot="label">
+        <i class="el-icon-user"></i>
+        数据条数
+      </template>
+      {{ this.$store.state.total }}
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template slot="label">
+        <i class="el-icon-mobile-phone"></i>
+        手机号
+      </template>
+      18100000000
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template slot="label">
+        <i class="el-icon-location-outline"></i>
+        居住地
+      </template>
+      苏州市
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template slot="label">
+        <i class="el-icon-tickets"></i>
+        备注
+      </template>
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template slot="label">
+        <i class="el-icon-office-building"></i>
+        联系地址
+      </template>
+      江苏省苏州市吴中区吴中大道 1188 号
+    </el-descriptions-item>
+  </el-descriptions>
+</template>
+<script>
+export default {
+  name: 'DataAnalyse',
+  data() {
+    return {}
+  },
+}
+</script>
+<style scoped>
+</style>
--- dbms/src/views/regular-atmo-params/ACL.vue
+++ dbms/src/views/regular-atmo-params/ACL.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <search-form params="acl" ref="aclData"></search-form>
-    <el-button type="primary" @click="lazyLoading()">刷新</el-button>
+    <el-button type="primary" @click="lazyLoading()">查询</el-button>
+    <el-button type="primary" @click="flashList()">刷新</el-button>
     <el-table :data="tableData" style="width: 50%" height="450">
       <el-table-column prop="date" label="日期" width="180"> </el-table-column>
       <el-table-column prop="time" label="时间" width="180"> </el-table-column>
       <el-table-column prop="r0" label="r0"> </el-table-column>
     </el-table>
+    <data-analyse></data-analyse>
   </div>
 </template>
 
 <script>
 import SearchForm from '../../components/SearchForm.vue'
+import DataAnalyse from '../../components/DataAnalyse.vue'
 export default {
   name: 'ACL',
-  components: { SearchForm },
+  components: { SearchForm, DataAnalyse },
   data() {
     return {
       loading: false,
@@ -30,6 +33,7 @@ export default {
       checked: false,
       otherHeight: 0,
       pageHeight: 0,
+      modPage: 0,
     }
   },
   // mounted() {
@@ -43,10 +47,11 @@ export default {
   methods: {
     lazyLoading() {
       this.tableData = []
-      this.totalPage = this.$store.state.total
-      this.countTotal = 20
-      console.log(this.totalPage)
-      for (let i = 1; i <= this.countTotal; i++) {
+      this.totalPage = this.$store.state.total / 10
+      this.modPage = this.$store.state.total % 10
+      // this.countTotal = 20
+      // console.log(this.totalPage)
+      for (let i = 0; i < this.countTotal; i++) {
         this.tableData.push(this.$store.state.dataList[i])
       }
       let dom = document.querySelector('.el-table__body-wrapper')
@@ -56,25 +61,35 @@ export default {
         // if (scrollDistance <= 0) {  //分辨率问题，如果设置 100% ，滑倒最底部，scrollDistance的值 可能为 0.201 到 -0.201
         if (scrollDistance <= 1) {
           //等于0证明已经到底，可以请求接口
-          if (this.pagination.page >= this.totalPage) {
+          if (this.pagination.page > this.totalPage) {
             //判断是否到达底部
             // this.$message.warning("我~是有底线的 (～￣▽￣)～");
             console.log('我~是有底线的 (～￣▽￣)～')
           }
-          if (this.pagination.page < this.totalPage) {
+          if (this.pagination.page < this.totalPage - 1) {
             //当前页数小于总页数就请求
             this.pagination.page++ //当前页数自增
-
             var cIndex = this.countTotal + 10
             for (let i = this.countTotal + 1; i <= cIndex; i++) {
               this.tableData.push(this.$store.state.dataList[i])
             }
             this.countTotal += 10
-            //this.getData();
-            // this.append();
+          }
+          if (this.pagination.page == this.totalPage) {
+            this.pagination.page++ //当前页数自增
+            var ccIndex = this.countTotal + this.modPage
+            for (let i = this.countTotal + 1; i <= ccIndex; i++) {
+              this.tableData.push(this.$store.state.dataList[i])
+            }
+            this.countTotal += this.modPage
           }
         }
       })
+    },
+    flashList() {
+      this.$store.state.total = 0
+      this.$store.state.dataList = []
+      this.lazyLoading()
     },
   },
 }
